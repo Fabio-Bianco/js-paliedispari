b8b9454f8739d9cd7448270d92a14e2b3d2b33b5
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-02 00:45:49 +0100

    feat; function randomNumber and oddEvenCheck

--- Pari-Dispari Js/main.js
+++ Pari-Dispari Js/main.js
@@ -1,1 +1,10 @@
-console.log ('Ciao Fabio')
+// Funzione per generare un numero casuale da 1 a 5
+function randomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+// Funzione per verificare se un numero è pari o dispari
+function oddEvenCheck(num) {
+    return num % 2 === 0 ? "pari" : "dispari";
+}
+
