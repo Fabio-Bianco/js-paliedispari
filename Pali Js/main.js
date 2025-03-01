60bbaae814113f6f9032e6341de018d5187f47ac
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-02 00:14:22 +0100

    feat: if else

--- Pali Js/main.js
+++ Pali Js/main.js
@@ -1,12 +1,18 @@
-
-
 // Chiede all'utente di inserire una parola;
 const userInput = prompt("Inserisci una parola:");
 
-function isPalindrome (word){
-    word = word.toLowerCase()// Converte tutto in minuscolo per evitare errori;
+function isPalindrome(word) {
+  word = word.toLowerCase(); // Converte tutto in minuscolo per evitare errori;
 
-    return word === word.split ('').reverse().join('') //Inverte la parola e confronta con l'originale;
+  return word === word.split("").reverse().join(""); //Inverte la parola e confronta con l'originale;
 }
-
-
+// Istruzioni condizionali;
+if (userInput) {
+  if (isPalindrome(userInput)) {
+    alert("La parola è palindroma!");
+  } else {
+    alert("La parola NON è palindroma!");
+  }
+} else {
+  alert("Inserisci una parola valida.");
+}
