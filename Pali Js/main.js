2865516f649ebbc66b1a14713be5332be71b56b7
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-02 00:05:31 +0100

    feat: prompt and funcrion

--- Pali Js/main.js
+++ Pali Js/main.js
@@ -1,1 +1,12 @@
-console.log ('Ciao Fabio')
+
+
+// Chiede all'utente di inserire una parola;
+const userInput = prompt("Inserisci una parola:");
+
+function isPalindrome (word){
+    word = word.toLowerCase()// Converte tutto in minuscolo per evitare errori;
+
+    return word === word.split ('').reverse().join('') //Inverte la parola e confronta con l'originale;
+}
+
+
